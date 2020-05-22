b42026fa709842e02a721afccd18fe0f387d1c4f
Alex Taghavi <spamdevil@protonmail.com>
2020-05-22 20:41:34 +0200

    Create about page
    - Add additional svg icons from fontawesome, modify index.js to import only what is needed
    - Modify origin.scss to add additional styling
    - Create about page including all its styling

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,12 @@ import ".././style/main.scss";
 import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
 import { faHome,  faUser, faList, faBriefcase, faEnvelope, faComments, faCode} from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
 
 import {test} from './app';
 
 
-library.add(faHome, faUser, faList, faBriefcase, faEnvelope, faComments, faTwitter, faCode, faLinkedin, faGithub);
+library.add(faHome, faUser, faList, faBriefcase, faEnvelope, faComments, faTwitter, faCode, faLinkedin, faGithub, faCalendarAlt);
 dom.watch()
 
 
